5ee5bfadb2835d51f68ccfdcab3b0cc2d8ed0edb
Bertrand Rigaldies <brigaldies@gmail.com>
2024-03-17 08:27:42 -0400

    Exit the shell script run_examples.sh if one example fails.

--- run_examples.sh
+++ run_examples.sh
@@ -1,5 +1,7 @@
-# Exit if an example fails.
-set -e
+#!/bin/bash
+
+# Error if unbound variable
+set -u
 
 examples=("type_hints" "async_io" "generators" "decorators" "sql_database" "interface")
 
@@ -7,7 +9,15 @@ for i in "${examples[@]}"
 do
   echo "*****************************"
   echo "Running example $i:"
+  set -x
   poetry run python main.py --example "$i" --debug true
   exitcode=$?
-  echo "Example $i exit code: $exitcode"
+  set +x
+  if [ $exitcode -eq 0 ]
+  then
+    echo "Example $i ran successfully"
+  else
+    echo "Example $i failed with exit code $exitcode"
+    exit 1
+  fi
 done
